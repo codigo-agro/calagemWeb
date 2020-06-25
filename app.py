73fe3e200b5c20d9708ea3d3f6500ab496ac8c39
Danilo Andrade <danilo_as@live.com>
2020-06-25 14:21:17 -0300

    Primeiro envio de arquivos

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from functions import *
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/')
-def index():
+@app.route('/home')
+def home():
     return render_template('home.html')
+
+@app.route('/page01')
+def page01():
+    return render_template('page01.html')
+
+@app.route('/page02')
+def page02():
+    return render_template('page02.html')
 
 @app.route('/send.data', methods = ['POST'])
 def send_data():
@@ -17,13 +25,12 @@ def send_data():
     sodio = request.form['sodio']
     hidrogeniomaisaluminio = request.form['hidrogenio-mais-aluminio']
     saturacaoporbases = request.form['saturacao-por-bases']
-    camada = request.form['camada-de-solo']
 
     # OUTPUT
     if calcio == '' or magnesio == '' or potassio == '' or sodio == '' or hidrogeniomaisaluminio == '' \
-            or saturacaoporbases == '' or camada == '':
-        prognostico_de_calagem = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
-        return render_template('home.html', prognostico_de_calagem=prognostico_de_calagem)
+            or saturacaoporbases == '':
+        resultado_sb = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
+        return render_template('page01.html', resultado_sb=resultado_sb)
 
     else:
         calcio = calcio.replace(',', '.')
@@ -32,7 +39,6 @@ def send_data():
         sodio = sodio.replace(',', '.')
         hidrogeniomaisaluminio = hidrogeniomaisaluminio.replace(',', '.')
         saturacaoporbases = saturacaoporbases.replace(',', '.')
-        camada = camada.replace(',', '.')
 
         calcio = float(calcio)
         magnesio = float(magnesio)
@@ -40,21 +46,65 @@ def send_data():
         sodio = float(sodio)
         hidrogeniomaisaluminio = float(hidrogeniomaisaluminio)
         saturacaoporbases = float(saturacaoporbases)
-        camada = float(camada)
 
-        necessidade_de_calagem = str(calcula_necessidade_de_calagem(calcio, magnesio, potassio, sodio,
+        necessidade_de_calagem = str(calcula_sb(calcio, magnesio, potassio, sodio,
                                                                     hidrogeniomaisaluminio,
-                                                                    saturacaoporbases,
-                                                                    camada))
+                                                                    saturacaoporbases,))
 
         quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
 
-        prognostico_de_calagem = str(
-            'Considerando uma aplicação em área total, recomenda-se para uma profundidade de ' +
-            str(round(camada, 0)) + ' cm, aplicar um valor equivalente à ' +
-            quantidade_de_calagem + ' t/ha')
+        resultado_sb = str(
+            'Considerando uma aplicação em área total, a necessidade de calagem é igual à ' + quantidade_de_calagem + ' '
+            't/ha.')
 
-        return render_template('home.html', prognostico_de_calagem=prognostico_de_calagem)
+        complemento = str('O cálculo final da quantidade de calcário ainda deve levar em conta o '
+            'tipo de aplicação - que pode ser em sulco, na projeção da copa da planta, área total ou cova - '
+            ' a profundidade de aplicação e o PRNT do calcário que será utilizado.')
+
+        return render_template('page01.html', resultado_sb=resultado_sb, complemento=complemento)
+
+@app.route('/page.al', methods = ['POST'])
+def page_al():
+    #INPUT
+    calcio = request.form['calcio']
+    magnesio = request.form['magnesio']
+    aluminio = request.form['aluminio']
+    ipsilon = request.form['ipsilon']
+    chis = request.form['chis']
+
+    # OUTPUT
+    if calcio == '' or magnesio == '' or aluminio == '' or ipsilon == '' or chis == '':
+        resultado_al = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
+        return render_template('page02.html', resultado_al=resultado_al)
+
+    else:
+        calcio = calcio.replace(',', '.')
+        magnesio = magnesio.replace(',', '.')
+        aluminio = aluminio.replace(',', '.')
+        ipsilon = ipsilon.replace(',', '.')
+        chis = chis.replace(',', '.')
+
+        calcio = float(calcio)
+        magnesio = float(magnesio)
+        aluminio = float(aluminio)
+        ipsilon = float(ipsilon)
+        chis = float(chis)
+
+        necessidade_de_calagem = str(calcula_al(calcio, magnesio, aluminio, ipsilon,
+                                                                    chis))
+
+        quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
+
+        resultado_al = str(
+            'Considerando uma aplicação em área total, a necessidade de calagem é igual à ' + quantidade_de_calagem + ' '
+            't/ha.')
+
+        complemento = str('O cálculo final da quantidade de calcário ainda deve levar em conta o '
+                   'tipo de aplicação - que pode ser em sulco, na projeção da copa da planta, área total ou cova - '
+                   ' a profundidade de aplicação e o PRNT do calcário que será utilizado.')
+
+        return render_template('page02.html', resultado_al=resultado_al, complemento=complemento)
+
 
 if (__name__ == '__main__'):
     app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,5 +1,18 @@
-def calcula_necessidade_de_calagem(ca, mg, k, na, hal, v1, pf):
+def calcula_sb(ca, mg, k, na, hal, v1):
     ctctotal = ca + mg + (k/390) + (na/230) + hal
     sb = ca + mg + (k/390) + (na/230)
     v2 = sb/ctctotal*100
-    return round(((ctctotal * (v1 - v2))/100 * (pf/20)), 2)
+    return round(((ctctotal * (v1 - v2))/100), 2)
+
+
+def calcula_al(ca, mg, al, y, x):
+    parte01 = y * al
+    parte02 = x - (ca + mg)
+    if parte01 == 0 and parte02 > 0:
+        return round(parte02, 2)
+    elif parte02 <= 0 and parte01 != 0:
+        return round(parte01, 2)
+    elif parte01 == 0 and parte02 <=0:
+        return 'Está amostra não precisa de calagem segundo o método de neutralização de Alíminio'
+    else:
+        return round((parte01 + parte02), 2)
