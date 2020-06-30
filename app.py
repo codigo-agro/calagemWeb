17424be7b358eb5839eb931fca58396ad5114eda
Danilo Andrade <danilo_as@live.com>
2020-06-30 16:00:59 -0300

    Envio de arquivos com a aplicação funcionando porém com o código desorganizado

--- app.py
+++ app.py
@@ -16,6 +16,22 @@ def page01():
 def page02():
     return render_template('page02.html')
 
+@app.route('/page03')
+def page03():
+    return render_template('page03.html')
+
+@app.route('/page04')
+def page04():
+    return render_template('page04.html')
+
+@app.route('/page05')
+def page05():
+    return render_template('page05.html')
+
+@app.route('/tabela')
+def tabela():
+    return render_template('tabela.html')
+
 @app.route('/send.data', methods = ['POST'])
 def send_data():
     #INPUT
@@ -49,7 +65,7 @@ def send_data():
 
         necessidade_de_calagem = str(calcula_sb(calcio, magnesio, potassio, sodio,
                                                                     hidrogeniomaisaluminio,
-                                                                    saturacaoporbases,))
+                                                                    saturacaoporbases))
 
         quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
 
@@ -69,29 +85,36 @@ def page_al():
     calcio = request.form['calcio']
     magnesio = request.form['magnesio']
     aluminio = request.form['aluminio']
-    ipsilon = request.form['ipsilon']
+    argila = request.form['argila']
     chis = request.form['chis']
+    prem = request.form['prem']
 
     # OUTPUT
-    if calcio == '' or magnesio == '' or aluminio == '' or ipsilon == '' or chis == '':
+    if calcio == '' or magnesio == '' or aluminio == '' or chis == '' or (prem == '' and argila == ''):
         resultado_al = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
         return render_template('page02.html', resultado_al=resultado_al)
 
     else:
+        if prem == '':
+            prem = str('1.0')
+        elif argila == '':
+            argila = str('1.0')
+
         calcio = calcio.replace(',', '.')
         magnesio = magnesio.replace(',', '.')
         aluminio = aluminio.replace(',', '.')
-        ipsilon = ipsilon.replace(',', '.')
+        argila_n = argila.replace(',', '.')
         chis = chis.replace(',', '.')
+        prem_n = prem.replace(',', '.')
 
         calcio = float(calcio)
         magnesio = float(magnesio)
         aluminio = float(aluminio)
-        ipsilon = float(ipsilon)
+        argila_n = float(argila_n)
         chis = float(chis)
+        prem_n = float(prem_n)
 
-        necessidade_de_calagem = str(calcula_al(calcio, magnesio, aluminio, ipsilon,
-                                                                    chis))
+        necessidade_de_calagem = str(calcula_al(calcio, magnesio, aluminio, chis, argila_n, prem_n))
 
         quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
 
@@ -105,6 +128,130 @@ def page_al():
 
         return render_template('page02.html', resultado_al=resultado_al, complemento=complemento)
 
+@app.route('/page.alm', methods = ['POST'])
+def page_alm():
+    #INPUT
+    calcio = request.form['calcio']
+    magnesio = request.form['magnesio']
+    potassio = request.form['potassio']
+    sodio = request.form['sodio']
+    aluminio = request.form['aluminio']
+    maxsatu = request.form['maxima-sat-aluminio']
+    argila = request.form['argila']
+    chis = request.form['chis']
+    prem = request.form['prem']
+
+    # OUTPUT
+    if calcio == '' or magnesio == '' or potassio == '' or sodio == '' or aluminio == '' or maxsatu == '' or chis == '' \
+            or (prem == '' and argila == ''):
+        resultado_alm = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
+        return render_template('page03.html', resultado_alm=resultado_alm)
+
+    else:
+        if prem == '':
+            prem = str('1.0')
+        elif argila == '':
+            argila = str('1.0')
+        calcio = calcio.replace(',', '.')
+        magnesio = magnesio.replace(',', '.')
+        potassio = potassio.replace(',', '.')
+        sodio = sodio.replace(',', '.')
+        aluminio = aluminio.replace(',', '.')
+        maxsatu = maxsatu.replace(',', '.')
+        argila_n = argila.replace(',', '.')
+        chis = chis.replace(',', '.')
+        prem_n = prem.replace(',', '.')
+
+        calcio = float(calcio)
+        magnesio = float(magnesio)
+        potassio = float(potassio)
+        sodio = float(sodio)
+        aluminio = float(aluminio)
+        maxsatu = float(maxsatu)
+        argila_n = float(argila_n)
+        chis = float(chis)
+        prem_n = float(prem_n)
+
+        necessidade_de_calagem = str(calcula_alm(calcio, magnesio,potassio, sodio, aluminio, maxsatu, chis, argila_n, prem_n))
+
+        quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
+
+        resultado_alm = str(
+            'Considerando uma aplicação em área total, a necessidade de calagem é igual à ' + quantidade_de_calagem + ' '
+            't/ha.')
+
+        complemento = str('O cálculo final da quantidade de calcário ainda deve levar em conta o '
+                   'tipo de aplicação - que pode ser em sulco, na projeção da copa da planta, área total ou cova - '
+                   ' a profundidade de aplicação e o PRNT do calcário que será utilizado.')
+
+        return render_template('page03.html', resultado_alm=resultado_alm, complemento=complemento)
+
+
+@app.route('/page.gc', methods = ['POST'])
+def page_gc():
+    #INPUT
+    calcio = request.form['calcio']
+    magnesio = request.form['magnesio']
+    potassio = request.form['potassio']
+    sodio = request.form['sodio']
+    aluminio = request.form['aluminio']
+    hidrogeniomaisaluminio = request.form['hidrogenio-mais-aluminio']
+    saturacaoporbases = request.form['saturacao-por-bases']
+    maxsatu = request.form['maxima-sat-aluminio']
+    argila = request.form['argila']
+    chis = request.form['chis']
+    prem = request.form['prem']
+
+    # OUTPUT
+    if calcio == '' or magnesio == '' or chis == '' or potassio == '' or sodio == '' or aluminio == ''\
+            or hidrogeniomaisaluminio == '' or saturacaoporbases == '' or maxsatu == '' or chis == '' \
+            or (prem == '' and argila == ''):
+        resultado_gc = str("Você deixou campos vazios no formulário. Tente novamente sempre digitando um número")
+        return render_template('page04.html', resultado_gc=resultado_gc)
+
+    else:
+        if prem == '':
+            prem = str('1.0')
+        elif argila == '':
+            argila = str('1.0')
+        calcio = calcio.replace(',', '.')
+        magnesio = magnesio.replace(',', '.')
+        potassio = potassio.replace(',', '.')
+        sodio = sodio.replace(',', '.')
+        aluminio = aluminio.replace(',', '.')
+        hidrogeniomaisaluminio = hidrogeniomaisaluminio.replace(',', '.')
+        saturacaoporbases = saturacaoporbases.replace(',', '.')
+        maxsatu = maxsatu.replace(',', '.')
+        argila_n = argila.replace(',', '.')
+        chis = chis.replace(',', '.')
+        prem_n = prem.replace(',', '.')
+
+        calcio = float(calcio)
+        magnesio = float(magnesio)
+        potassio = float(potassio)
+        sodio = float(sodio)
+        aluminio = float(aluminio)
+        hidrogeniomaisaluminio = float(hidrogeniomaisaluminio)
+        saturacaoporbases = float(saturacaoporbases)
+        maxsatu = float(maxsatu)
+        argila_n = float(argila_n)
+        chis = float(chis)
+        prem_n = float(prem_n)
+
+        necessidade_de_calagem = str(guarconi_e_sobreira(calcio, magnesio, potassio, sodio, aluminio, hidrogeniomaisaluminio,
+                                                     saturacaoporbases, maxsatu, chis, argila_n, prem_n))
+
+        quantidade_de_calagem = necessidade_de_calagem.replace('.', ',')
+
+        resultado_gc = str(
+            'Considerando uma aplicação em área total, a necessidade de calagem é igual à ' + quantidade_de_calagem + ' '
+            't/ha.')
+
+        complemento = str('O cálculo final da quantidade de calcário ainda deve levar em conta o '
+                   'tipo de aplicação - que pode ser em sulco, na projeção da copa da planta, área total ou cova - '
+                   ' a profundidade de aplicação e o PRNT do calcário que será utilizado.')
+
+        return render_template('page04.html', resultado_gc=resultado_gc, complemento=complemento)
 
 if (__name__ == '__main__'):
     app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,18 +1,141 @@
-def calcula_sb(ca, mg, k, na, hal, v1):
+def calcula_sb(ca, mg, k, na, hal, v2):
     ctctotal = ca + mg + (k/390) + (na/230) + hal
     sb = ca + mg + (k/390) + (na/230)
-    v2 = sb/ctctotal*100
-    return round(((ctctotal * (v1 - v2))/100), 2)
-
-
-def calcula_al(ca, mg, al, y, x):
-    parte01 = y * al
-    parte02 = x - (ca + mg)
-    if parte01 == 0 and parte02 > 0:
-        return round(parte02, 2)
-    elif parte02 <= 0 and parte01 != 0:
-        return round(parte01, 2)
-    elif parte01 == 0 and parte02 <=0:
-        return 'Está amostra não precisa de calagem segundo o método de neutralização de Alíminio'
+    v1 = sb/ctctotal*100
+    if v1 >= v2:
+        return 0
     else:
-        return round((parte01 + parte02), 2)
+        return round(((ctctotal * (v2 - v1))/100), 2)
+
+def calcula_al(ca, mg, al, x, arg, prem):
+    if prem != '':
+        y = 4.002 - (0.125901 * prem) + (0.001205 * (prem**2)) - (0.00000362 * (prem ** 3))
+        parte01 = y * al
+        parte02 = x - (ca + mg)
+        if parte01 <= 0 <= parte02:
+            return round(parte02, 2)
+        elif parte02 <= 0 <= parte01:
+            return round(parte01, 2)
+        elif parte01 <= 0 and parte02 <= 0:
+            return 0
+        else:
+            return round((parte01 + parte02), 2)
+    elif arg != '':
+        y = 0.0302 + (0.06532 * arg) - (0.000257 * (arg ** 2))
+        parte01 = y * al
+        parte02 = x - (ca + mg)
+        if parte01 <= 0 <= parte02:
+            return round(parte02, 2)
+        elif parte02 <= 0 <= parte01:
+            return round(parte01, 2)
+        elif parte01 <= 0 and parte02 <= 0:
+            return 0
+        else:
+            return round((parte01 + parte02), 2)
+
+def calcula_alm(ca, mg, k, na, al, mt, x, prem, arg):
+    if prem != '':
+        y = 4.002 - (0.125901 * prem) + (0.001205 * (prem ** 2)) - (0.00000362 * (prem ** 3))
+        t = ca + mg + (k/390) + (na/230) + al
+        parte01 = y * (al - (mt * t/100))
+        parte02 = x - (ca + mg)
+        if parte01 <= 0 <= parte02:
+            return round(parte02, 2)
+        elif parte02 <= 0 <= parte01:
+            return round(parte01, 2)
+        elif parte01 <= 0 and parte02 <= 0:
+            return 0
+        else:
+            return round((parte01 + parte02), 2)
+    elif arg != '':
+        y = 0.0302 + (0.06532 * arg) - (0.000257 * (arg ** 2))
+        t = ca + mg + (k / 390) + (na / 230) + al
+        parte01 = y * (al - (mt * t / 100))
+        parte02 = x - (ca + mg)
+        if parte01 <= 0 <= parte02:
+            return round(parte02, 2)
+        elif parte02 <= 0 <= parte01:
+            return round(parte01, 2)
+        elif parte01 <= 0 and parte02 <= 0:
+            return 0
+        else:
+            return round((parte01 + parte02), 2)
+
+def guarconi_e_sobreira(ca, mg, k, na, al, hal, v1, mt, x, prem, arg):
+    if prem != '':
+        y = 4.002 - (0.125901 * prem) + (0.001205 * (prem ** 2)) - (0.00000362 * (prem ** 3))
+        ctctotal = ca + mg + (k / 390) + (na / 230) + hal
+        t = ca + mg + (k / 390) + (na / 230) + al
+        sb = ca + mg + (k / 390) + (na / 230)
+        v2 = sb / ctctotal * 100
+        ncsb = (ctctotal * (v1 - v2)) / 100
+
+        parte01 = y * (al - (mt * t / 100))
+        parte02 = x - (ca + mg)
+
+        met01 = parte01 + parte02
+        met02 = ncsb
+
+        if met02 < 0:
+            return 0
+        elif met02 <= met01 or (met02 >= 0 > met01):
+            if met02 < parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
+            elif met02 >= parte02:
+                if met02 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met02, 2)
+        elif met01 < met02:
+            if met01 <= parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
+            elif met01 > parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
+
+    elif arg != '':
+        y = 0.0302 + (0.06532 * arg) - (0.000257 * (arg ** 2))
+        ctctotal = ca + mg + (k / 390) + (na / 230) + hal
+        t = ca + mg + (k / 390) + (na / 230) + al
+        sb = ca + mg + (k / 390) + (na / 230)
+        v2 = sb / ctctotal * 100
+        ncsb = (ctctotal * (v1 - v2)) / 100
+
+        parte01 = y * (al - (mt * t / 100))
+        parte02 = x - (ca + mg)
+
+        met01 = parte01 + parte02
+        met02 = ncsb
+
+        if met02 < 0:
+            return 0
+        elif met02 <= met01 or (met02 >= 0 > met01):
+            if met02 < parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
+            elif met02 >= parte02:
+                if met02 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met02, 2)
+        elif met01 < met02:
+            if met01 <= parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
+            elif met01 > parte02:
+                if met01 > ctctotal:
+                    return round(ctctotal, 2)
+                else:
+                    return round(met01, 2)
